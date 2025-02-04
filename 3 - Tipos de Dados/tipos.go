1758184c9214e20908d03d5b56570f106be42791
drArthurpts <arthurpatriciosousa@gmail.com>
2025-02-04 19:42:51 -0300

    Tipos de dados

---
+++ 3 - Tipos de Dados/tipos.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numero int = 100
+	fmt.Println(numero)
+
+	var numero2 uint32 = 100000000
+	fmt.Println(numero2)
+
+	// alias
+	var numero3 rune = 1245
+	fmt.Println(numero3)
+
+	float32 := 49.99
+	fmt.Println(float32)
+	
+	float64 := 4900000000000.99
+	fmt.Println(float64)
+}
